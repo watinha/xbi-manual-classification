85f6d1a637664eb74b79b8d32bc835a4d769f58d
Willian Watanabe <watinha@gmail.com>
2021-08-05 16:45:51 -0300

    will be using views in this project

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const path = require('path'),
       express = require('express'),
       app = express();
 
-app.set('views', path.join(__dirname, 'view'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
