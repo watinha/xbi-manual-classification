7538d657bd94190f5a07f010c71eebfc9aa22c86
Willian Watanabe <watinha@gmail.com>
2021-08-05 17:11:20 -0300

    first commit for frontend structure

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
+};
