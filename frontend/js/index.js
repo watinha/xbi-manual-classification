a4e1e0406f47e659cd940c025b7bc19de32ea7e3
Willian Watanabe <watinha@gmail.com>
2021-08-10 16:56:18 -0300

    loading complete screenshots for each comparison

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,14 +3,16 @@ import Loader from './loader.js';
 
 const ELEMENTS = {
         CLASSIFIED_COUNT: document.querySelector('#classified_count'),
-        UNCLASSIFIED_COUNT: document.querySelector('#unclassified_count')
+        UNCLASSIFIED_COUNT: document.querySelector('#unclassified_count'),
+        SCREENSHOTS_DIV: document.querySelector('.screenshots')
       },
       URLS = {
         CLASSIFIED: './classified',
         UNCLASSIFIED: './unclassified'
       };
 
-let renderer = Renderer([
+let cursor = 0,
+    renderer = Renderer([
   Loader({
     url: URLS.CLASSIFIED,
     target: ELEMENTS.CLASSIFIED_COUNT,
@@ -20,6 +22,23 @@ let renderer = Renderer([
     url: URLS.UNCLASSIFIED,
     target: ELEMENTS.UNCLASSIFIED_COUNT,
     mapping: (data) => data.length
+  }),
+  Loader({
+    url: [URLS.UNCLASSIFIED, '/', cursor].join(''),
+    target: ELEMENTS.SCREENSHOTS_DIV,
+    mapping: (data) => {
+      const imgBase = document.createElement("img"),
+            imgTarget = document.createElement("img"),
+            baseContainer = document.createElement("div"),
+            targetContainer = document.createElement("div");
+      imgBase.src = data.baseScreenshot.replace(/\d+\.png/, 'complete.png')
+      imgTarget.src = data.targetScreenshot.replace(/\d+\.png/, 'complete.png')
+
+      baseContainer.appendChild(imgBase);
+      targetContainer.appendChild(imgTarget);
+
+      return [baseContainer.outerHTML, targetContainer.outerHTML].join('');
+    }
   })
 ]);
 
---
+++ public/generate_arff.py
@@ -0,0 +1,29 @@
+import arff
+
+f = open('arffs.txt')
+
+filenames = f.read().split('\n')
+f.close()
+
+complete_dataset = None
+
+filenames = [filename for filename in filenames if len(filename) > 0]
+
+for filename in filenames:
+    dataset = arff.load(open(filename))
+    [folder, file_path] = filename.split('results/')
+
+    baseScreenshot_index = dataset['attributes'].index(('baseScreenshot', 'STRING'))
+    targetScreenshot_index = dataset['attributes'].index(('targetScreenshot', 'STRING'))
+    for row in dataset['data']:
+        row[baseScreenshot_index] = ('%s%s' % (folder, row[baseScreenshot_index]))
+        row[targetScreenshot_index] = ('%s%s' % (folder, row[targetScreenshot_index]))
+
+    if complete_dataset is None:
+        complete_dataset = dataset
+    else:
+        complete_dataset['data'] += dataset['data']
+
+new_f = open('./dataset.unclassified.arff', 'wt')
+new_f.write(arff.dumps(complete_dataset))
+new_f.close()
