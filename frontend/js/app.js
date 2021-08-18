7f16bd1540815ceacd1bf02a92427994e3a3ed02
watinha <watinha@gmail.com>
2021-08-17 22:13:11 -0300

    classifying and saving commands in the app.js frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+
+const CLASSIFY_URL = './classified';
+
 const App = ({ renderer, max=100000 }) => {
   let cursor = 0;
 
@@ -5,16 +9,27 @@ const App = ({ renderer, max=100000 }) => {
 
   const self = {
     get_cursor: () => cursor,
+
     set_cursor: (v) => {
       cursor = Math.max(
         Math.min(v, max), 0);
       renderer.render();
     },
+
     next: () => {
       self.set_cursor(cursor + 1);
     },
+
     previous: () => {
       self.set_cursor(cursor - 1);
+    },
+
+    classify: (data) => {
+      axios.post(CLASSIFY_URL, { action: 'add', data });
+    },
+
+    save: () => {
+      axios.post(CLASSIFY_URL, { action: 'save' });
     }
   };
 
--- test/frontend/app.test.js
+++ test/frontend/app.test.js
@@ -6,109 +6,160 @@ import axios from 'axios';
 
 import App from '../../frontend/js/app.js';
 
+jest.mock('axios');
+
 describe('app', () => {
 
-  it('should call renderer.render when initialized', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock });
+  describe('cursor handling', () => {
 
-    expect(renderer_mock.render).toHaveBeenCalled();
+    it('should call renderer.render when initialized', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock });
+
+      expect(renderer_mock.render).toHaveBeenCalled();
+    });
+
+    it('should initialize get_cursor with 0', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock });
+
+      expect(app.get_cursor()).toBe(0);
+    });
+
+    it('should increment cursor, after next is called', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock });
+
+      app.next();
+      expect(app.get_cursor()).toBe(1);
+      expect(renderer_mock.render.mock.calls.length).toBe(2);
+      app.next();
+      expect(app.get_cursor()).toBe(2);
+      expect(renderer_mock.render.mock.calls.length).toBe(3);
+    });
+
+    it('should not increment cursor, if max limit is reached', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock, max: 3 });
+
+      app.next();
+      expect(renderer_mock.render.mock.calls.length).toBe(2);
+      app.next();
+      expect(renderer_mock.render.mock.calls.length).toBe(3);
+      app.next();
+      expect(renderer_mock.render.mock.calls.length).toBe(4);
+      app.next();
+      expect(app.get_cursor()).toBe(3);
+      expect(renderer_mock.render.mock.calls.length).toBe(5);
+    });
+
+    it('should set cursor to an initial position', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock });
+
+      app.set_cursor(99);
+      expect(app.get_cursor()).toBe(99);
+      expect(renderer_mock.render.mock.calls.length).toBe(2);
+    });
+
+    it('should set cursor to another position', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock });
+
+      app.set_cursor(72);
+      expect(app.get_cursor()).toBe(72);
+      expect(renderer_mock.render.mock.calls.length).toBe(2);
+    });
+
+    it('should not set cursor beyound max', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock, max: 2 });
+
+      app.set_cursor(77);
+      expect(app.get_cursor()).toBe(2);
+      expect(renderer_mock.render.mock.calls.length).toBe(2);
+    });
+
+    it('should not set cursor to lower than 0', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock, max: 2 });
+
+      app.set_cursor(-3);
+      expect(app.get_cursor()).toBe(0);
+      expect(renderer_mock.render.mock.calls.length).toBe(2);
+    });
+
+    it('should decrement cursor when previous is pressed', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock, max: 33 });
+
+      app.set_cursor(33);
+      app.previous();
+
+      expect(app.get_cursor()).toBe(32);
+      expect(renderer_mock.render.mock.calls.length).toBe(3);
+
+      app.previous();
+
+      expect(app.get_cursor()).toBe(31);
+      expect(renderer_mock.render.mock.calls.length).toBe(4);
+    });
+
+    it('should not decrement cursor if 0 is reached', () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock, max: 33 });
+
+      app.previous();
+
+      expect(app.get_cursor()).toBe(0);
+      expect(renderer_mock.render.mock.calls.length).toBe(2);
+    });
+
   });
 
-  it('should initialize get_cursor with 0', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock });
+  describe('classifying and saving', () => {
 
-    expect(app.get_cursor()).toBe(0);
-  });
+    it('should send a classified row to the server', async () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock, max: 33 }),
+            row_stub = { external: 1, internal: 1 };
 
-  it('should increment cursor, after next is called', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock });
+      axios.post = jest.fn();
+      axios.post.mockResolvedValue({ message: 'success! yay' });
 
-    app.next();
-    expect(app.get_cursor()).toBe(1);
-    expect(renderer_mock.render.mock.calls.length).toBe(2);
-    app.next();
-    expect(app.get_cursor()).toBe(2);
-    expect(renderer_mock.render.mock.calls.length).toBe(3);
-  });
+      app.classify(row_stub);
 
-  it('should not increment cursor, if max limit is reached', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock, max: 3 });
+      expect(axios.post).toHaveBeenCalledWith('./classified', {
+        action: 'add', data: row_stub });
+    });
 
-    app.next();
-    expect(renderer_mock.render.mock.calls.length).toBe(2);
-    app.next();
-    expect(renderer_mock.render.mock.calls.length).toBe(3);
-    app.next();
-    expect(renderer_mock.render.mock.calls.length).toBe(4);
-    app.next();
-    expect(app.get_cursor()).toBe(3);
-    expect(renderer_mock.render.mock.calls.length).toBe(5);
-  });
+    it('should send a different classified row to the server', async () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock, max: 33 }),
+            row_stub = { external: 0, internal: 0 };
 
-  it('should set cursor to an initial position', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock });
+      axios.post = jest.fn();
+      axios.post.mockResolvedValue({ message: 'success! yay' });
 
-    app.set_cursor(99);
-    expect(app.get_cursor()).toBe(99);
-    expect(renderer_mock.render.mock.calls.length).toBe(2);
-  });
+      app.classify(row_stub);
 
-  it('should set cursor to another position', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock });
+      expect(axios.post).toHaveBeenCalledWith('./classified', {
+        action: 'add', data: row_stub });
+    });
 
-    app.set_cursor(72);
-    expect(app.get_cursor()).toBe(72);
-    expect(renderer_mock.render.mock.calls.length).toBe(2);
-  });
+    it('should send save action to server', async () => {
+      const renderer_mock = { render: jest.fn() },
+            app = App({ renderer: renderer_mock, max: 33 });
 
-  it('should not set cursor beyound max', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock, max: 2 });
+      axios.post = jest.fn();
+      axios.post.mockResolvedValue({ message: 'Arff file generated!!!' });
 
-    app.set_cursor(77);
-    expect(app.get_cursor()).toBe(2);
-    expect(renderer_mock.render.mock.calls.length).toBe(2);
-  });
+      app.save();
 
-  it('should not set cursor to lower than 0', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock, max: 2 });
+      expect(axios.post).toHaveBeenCalledWith('./classified', {
+        action: 'save' });
+    });
 
-    app.set_cursor(-3);
-    expect(app.get_cursor()).toBe(0);
-    expect(renderer_mock.render.mock.calls.length).toBe(2);
-  });
-
-  it('should decrement cursor when previous is pressed', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock, max: 33 });
-
-    app.set_cursor(33);
-    app.previous();
-
-    expect(app.get_cursor()).toBe(32);
-    expect(renderer_mock.render.mock.calls.length).toBe(3);
-
-    app.previous();
-
-    expect(app.get_cursor()).toBe(31);
-    expect(renderer_mock.render.mock.calls.length).toBe(4);
-  });
-
-  it('should not decrement cursor if 0 is reached', () => {
-    const renderer_mock = { render: jest.fn() },
-          app = App({ renderer: renderer_mock, max: 33 });
-
-    app.previous();
-
-    expect(app.get_cursor()).toBe(0);
-    expect(renderer_mock.render.mock.calls.length).toBe(2);
   });
 
 });
