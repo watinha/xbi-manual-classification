a99b9e0f2af1db8d0b2ca37c5e7cdb5f6fe47989
Willian Watanabe <watinha@gmail.com>
2021-09-29 21:46:07 -0300

    WIP: initiating App instance in the frontend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,4 @@
+import App from './app.js';
 import Renderer from './renderer.js';
 import Loader from './loader.js';
 import { render_complete_screenshot, render_metadata, render_screenshot } from './components/index.js';
@@ -9,16 +10,26 @@ const ELEMENTS = {
         BASE_CURSOR_DIV: document.querySelector('.cursor .base'),
         TARGET_CURSOR_DIV: document.querySelector('.cursor .target'),
         METADATA_TR: document.querySelector('.metadata'),
-        SCREENSHOTS_CONTAINER: document.querySelector('#container')
+        SCREENSHOTS_CONTAINER: document.querySelector('#container'),
+        INTERNAL_INPUT: document.querySelector('#internal_classifier'),
+        EXTERNAL_INPUT: document.querySelector('#external_classifier'),
+      },
+      KEYS = {
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40,
+        ENTER: 13
       },
       URLS = {
         CLASSIFIED: './classified',
         UNCLASSIFIED: './unclassified'
       };
 
-let cursor = 0;
+let cursor = 0,
+    current = null;
 
-let renderer = Renderer([
+const renderer = Renderer([
   Loader({
     url: URLS.CLASSIFIED,
     target: ELEMENTS.CLASSIFIED_COUNT,
@@ -30,26 +41,41 @@ let renderer = Renderer([
     mapping: (data) => data.length
   }),
   Loader({
-    url: `${URLS.UNCLASSIFIED}/${cursor}`,
+    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
     target: ELEMENTS.SCREENSHOTS_DIV,
-    mapping: render_complete_screenshot
-
+    mapping: (data) => {
+      current = data;
+      return render_complete_screenshot(data);
+    }
   }),
   Loader({
-    url: [URLS.UNCLASSIFIED, '/', cursor].join(''),
+    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
     target: ELEMENTS.BASE_CURSOR_DIV,
     mapping: (data) => render_screenshot(data.baseScreenshot)
   }),
   Loader({
-    url: [URLS.UNCLASSIFIED, '/', cursor].join(''),
+    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
     target: ELEMENTS.TARGET_CURSOR_DIV,
     mapping: (data) => render_screenshot(data.targetScreenshot)
   }),
   Loader({
-    url: [URLS.UNCLASSIFIED, '/', cursor].join(''),
+    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
     target: ELEMENTS.METADATA_TR,
     mapping: render_metadata
   })
 ]);
+
+const app = App({ renderer }),
+      internal_input = ELEMENTS.INTERNAL_INPUT,
+      external_input = ELEMENTS.EXTERNAL_INPUT;
+
+internal_input.focus();
+internal_input.addEventListener('keyup', (ev) => {
+  if ([KEYS.UP, KEYS.LEFT].indexOf(ev.keyCode) >= 0)
+    app.next();
+  if ([KEYS.DOWN, KEYS.RIGHT].indexOf(ev.keyCode) >= 0)
+    app.previous();
+  cursor = app.get_cursor();
+});
 
 renderer.render();
