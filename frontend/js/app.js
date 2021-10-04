5219d77f365d73139cc0d2f9f27e15618c617cd1
Willian Watanabe <watinha@gmail.com>
2021-10-04 16:34:57 -0300

    moving app logic to a separate file and element pointer is now working

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,64 @@
 import axios from 'axios';
 
-const CLASSIFY_URL = './classified';
+import Renderer from './renderer.js';
+import Loader from './loader.js';
+import { render_complete_screenshot, render_metadata, render_screenshot } from
+  './components/index.js';
 
-const App = ({ renderer, max=100000 }) => {
-  let cursor = 0;
+const ELEMENTS = {
+        CLASSIFIED_COUNT: document.querySelector('#classified_count'),
+        UNCLASSIFIED_COUNT: document.querySelector('#unclassified_count'),
+        SCREENSHOTS_DIV: document.querySelector('.screenshots'),
+        BASE_CURSOR_DIV: document.querySelector('.cursor .base'),
+        TARGET_CURSOR_DIV: document.querySelector('.cursor .target'),
+        METADATA_TR: document.querySelector('.metadata'),
+        SCREENSHOTS_CONTAINER: document.querySelector('#container'),
+      },
+      URLS = {
+        CLASSIFIED: './classified',
+        UNCLASSIFIED: './unclassified'
+      };
 
+let cursor = 0,
+    current = null;
+
+const renderer = Renderer([
+  Loader({
+    url: URLS.CLASSIFIED,
+    target: ELEMENTS.CLASSIFIED_COUNT,
+    mapping: (data) => data.length
+  }),
+  Loader({
+    url: URLS.UNCLASSIFIED,
+    target: ELEMENTS.UNCLASSIFIED_COUNT,
+    mapping: (data) => data.length
+  }),
+  Loader({
+    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
+    target: ELEMENTS.SCREENSHOTS_DIV,
+    mapping: (data) => {
+      current = data;
+      return render_complete_screenshot(data);
+    }
+  }),
+  Loader({
+    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
+    target: ELEMENTS.BASE_CURSOR_DIV,
+    mapping: (data) => render_screenshot(data.baseScreenshot)
+  }),
+  Loader({
+    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
+    target: ELEMENTS.TARGET_CURSOR_DIV,
+    mapping: (data) => render_screenshot(data.targetScreenshot)
+  }),
+  Loader({
+    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
+    target: ELEMENTS.METADATA_TR,
+    mapping: render_metadata
+  })
+]);
+
+const App = ({ max=100000 }) => {
   renderer.render();
 
   const self = {
@@ -25,11 +79,11 @@ const App = ({ renderer, max=100000 }) => {
     },
 
     classify: (id, data) => {
-      axios.put(`${CLASSIFY_URL}/${id}`, { data });
+      axios.put(`${URLS.CLASSIFIED}/${id}`, { data });
     },
 
     save: () => {
-      axios.post(CLASSIFY_URL);
+      axios.post(URLS.CLASSIFIED);
     }
   };
 
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,16 +1,6 @@
 import App from './app.js';
-import Renderer from './renderer.js';
-import Loader from './loader.js';
-import { render_complete_screenshot, render_metadata, render_screenshot } from './components/index.js';
 
 const ELEMENTS = {
-        CLASSIFIED_COUNT: document.querySelector('#classified_count'),
-        UNCLASSIFIED_COUNT: document.querySelector('#unclassified_count'),
-        SCREENSHOTS_DIV: document.querySelector('.screenshots'),
-        BASE_CURSOR_DIV: document.querySelector('.cursor .base'),
-        TARGET_CURSOR_DIV: document.querySelector('.cursor .target'),
-        METADATA_TR: document.querySelector('.metadata'),
-        SCREENSHOTS_CONTAINER: document.querySelector('#container'),
         INTERNAL_INPUT: document.querySelector('#internal_classifier'),
         EXTERNAL_INPUT: document.querySelector('#external_classifier'),
       },
@@ -20,62 +10,16 @@ const ELEMENTS = {
         RIGHT: 39,
         DOWN: 40,
         ENTER: 13
-      },
-      URLS = {
-        CLASSIFIED: './classified',
-        UNCLASSIFIED: './unclassified'
       };
 
-let cursor = 0,
-    current = null;
-
-const renderer = Renderer([
-  Loader({
-    url: URLS.CLASSIFIED,
-    target: ELEMENTS.CLASSIFIED_COUNT,
-    mapping: (data) => data.length
-  }),
-  Loader({
-    url: URLS.UNCLASSIFIED,
-    target: ELEMENTS.UNCLASSIFIED_COUNT,
-    mapping: (data) => data.length
-  }),
-  Loader({
-    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
-    target: ELEMENTS.SCREENSHOTS_DIV,
-    mapping: (data) => {
-      current = data;
-      return render_complete_screenshot(data);
-    }
-  }),
-  Loader({
-    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
-    target: ELEMENTS.BASE_CURSOR_DIV,
-    mapping: (data) => render_screenshot(data.baseScreenshot)
-  }),
-  Loader({
-    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
-    target: ELEMENTS.TARGET_CURSOR_DIV,
-    mapping: (data) => render_screenshot(data.targetScreenshot)
-  }),
-  Loader({
-    url: () => `${URLS.UNCLASSIFIED}/${cursor}`,
-    target: ELEMENTS.METADATA_TR,
-    mapping: render_metadata
-  })
-]);
-
-const app = App({ renderer }),
+const app = App({}),
       internal_input = ELEMENTS.INTERNAL_INPUT,
       external_input = ELEMENTS.EXTERNAL_INPUT;
 
 internal_input.focus();
 internal_input.addEventListener('keyup', (ev) => {
   if ([KEYS.UP, KEYS.LEFT].indexOf(ev.keyCode) >= 0)
-    app.next();
-  if ([KEYS.DOWN, KEYS.RIGHT].indexOf(ev.keyCode) >= 0)
     app.previous();
-  cursor = app.get_cursor();
+  if ([KEYS.DOWN, KEYS.RIGHT].indexOf(ev.keyCode) >= 0)
+    app.next();
 });
-
-renderer.render();
