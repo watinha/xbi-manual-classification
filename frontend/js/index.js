856257d3b86a7e13699a3a3b083cc2e05e0f7704
Willian Watanabe <watinha@gmail.com>
2021-11-12 15:20:47 -0300

    disable internal classification if the screenshot is not renderer

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,7 +4,9 @@ const ELEMENTS = {
         INTERNAL_INPUT: document.querySelector('#internal_classifier'),
         EXTERNAL_INPUT: document.querySelector('#external_classifier'),
         SCREENSHOTS_DIV: document.querySelector('.screenshots'),
-        GENERATE_ARFF_BUTTON: document.querySelector('.menu button')
+        GENERATE_ARFF_BUTTON: document.querySelector('.menu button'),
+        BASE_CURSOR_DIV: document.querySelector('.cursor .base'),
+        TARGET_CURSOR_DIV: document.querySelector('.cursor .target'),
       },
       SELECTORS = {
         BASELINE_SCREENSHOT: '.screenshots > div > img:first-child'
@@ -21,9 +23,28 @@ const app = App({}),
       internal_input = ELEMENTS.INTERNAL_INPUT,
       external_input = ELEMENTS.EXTERNAL_INPUT,
       screenshots_div = ELEMENTS.SCREENSHOTS_DIV,
-      generate_button = ELEMENTS.GENERATE_ARFF_BUTTON;
+      generate_button = ELEMENTS.GENERATE_ARFF_BUTTON,
+      base_cursor_div = ELEMENTS.BASE_CURSOR_DIV,
+      target_cursor_div = ELEMENTS.TARGET_CURSOR_DIV;
 
 internal_input.focus();
+setInterval(() => {
+  const base_img = base_cursor_div.querySelector('img'),
+        target_img = target_cursor_div.querySelector('img');
+  if ((base_img && base_img.src.search(/null$/) !== -1) ||
+      (target_img && target_img.src.search(/null$/) !== -1)) {
+    internal_input.value = '';
+    internal_input.disabled = true;
+    external_input.focus();
+  } else {
+    if (internal_input.disabled) {
+      internal_input.disabled = false;
+      internal_input.focus();
+    }
+    internal_input.disabled = false;
+  }
+}, 300);
+
 internal_input.addEventListener('keyup', (ev) => {
   if ([KEYS.UP, KEYS.LEFT].indexOf(ev.keyCode) >= 0)
     app.previous();
