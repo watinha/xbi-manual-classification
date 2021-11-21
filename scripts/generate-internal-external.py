becf644317cf445df0804a969df8325c3c19cb2c
Willian Watanabe <watinha@gmail.com>
2021-11-20 23:26:02 -0300

    fixes possibility of inf and none for mse metric as well

--- scripts/generate-internal-external.py
+++ scripts/generate-internal-external.py
@@ -8,6 +8,7 @@ arff_file = sys.argv[1]
 dataset = arff.load(open(arff_file))
 attributes = [attribute[0] for attribute in dataset['attributes']]
 max_psnr = -1
+max_mse = -1
 
 filtered = []
 structure_xbis = []
@@ -18,6 +19,12 @@ for row in dataset['data']:
         psnr = row[attributes.index('psnr')]
         if psnr != float('inf') and psnr > max_psnr:
             max_psnr = psnr
+        mse = row[attributes.index('mse')]
+        if mse is None:
+            mse = -1
+            row[attributes.index('mse')] = -1
+        if mse != float('inf') and mse > max_mse:
+            max_mse = mse
         filtered.append(row)
 
 
@@ -34,10 +41,17 @@ for row in filtered:
 
     if (row[attributes.index('internal')] == '1'): count_internal += 1
     if (row[attributes.index('external')] == '1'): count_external += 1
+
     psnr = row[attributes.index('psnr')]
     if psnr == float('inf'):
         row[attributes.index('psnr')] = max_psnr
         print('changing psnr from inf -> %d' % (max_psnr))
+
+    mse = row[attributes.index('mse')]
+    if mse == float('inf'):
+        row[attributes.index('mse')] = max_mse
+        print('changing mse from inf -> %d' % (max_mse))
+
 
     if row[attributes.index('childsNumber')] == 0.0 and base_platform != 'null' and target_platform != 'null' and not base_path.endswith('/null') and not target_path.endswith('/null'):
         internal.append(row)
